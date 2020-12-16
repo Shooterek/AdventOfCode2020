0d7ae4a426bf9f16940ed9f102d13ad8823f1eb3
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2020-12-16 22:47:01 +0100

    Day 16 part two.

--- Days/Day16.cs
+++ Days/Day16.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public class Day16 : Day
 {
     public Day16(string inputPath) : base(inputPath)
@@ -29,6 +33,55 @@ public class Day16 : Day
 
     public override string SecondTask()
     {
-        throw new System.NotImplementedException();
+        var input = _inputLoader.LoadTicketRules(_inputPath);
+        var invalidTickets = new List<List<int>>();
+        foreach(var ticket in input.NearbyTickets){
+            foreach(var ticketValue in ticket){
+                var canBeValid = false;
+                foreach(var rule in input.TicketRules){
+                    if(rule.CheckIfValueIsValid(ticketValue)){
+                        canBeValid = true;
+                        break;
+                    }
+                }
+                if(canBeValid == false){
+                    invalidTickets.Add(ticket);
+                }
+            }
+        }
+
+        foreach(var ticket in invalidTickets){
+            input.NearbyTickets.Remove(ticket);
+        }
+
+        long returnValue = 1;
+        while(input.TicketRules.Count > 0){
+            for(int i = 0; i < input.NearbyTickets[0].Count; i++){
+                var validRulesCount = new Dictionary<int, int>();
+                for(int b = 0; b < input.TicketRules.Count; b++){
+                    validRulesCount[b] = 0;
+                }
+                for(int j = 0; j < input.TicketRules.Count; j++){
+                    var rule = input.TicketRules[j];
+                    for(int z = 0; z < input.NearbyTickets.Count; z++){
+                        if(rule.CheckIfValueIsValid(input.NearbyTickets[z][i])){
+                            validRulesCount[j] = validRulesCount[j] + 1; 
+                        }
+                    }
+                }
+
+                if(validRulesCount.Values.Where(v => v == input.NearbyTickets.Count).Count() == 1){
+                    var rule = validRulesCount.First(x => x.Value == input.NearbyTickets.Count);
+                    Console.WriteLine(input.TicketRules[rule.Key].PropertyName + " " + i);
+                    if(input.TicketRules[rule.Key].PropertyName.Contains("departure")){
+                        returnValue *= input.MyTicket[i];
+                    }
+                    input.TicketRules.RemoveAt(rule.Key);
+                }
+            }
+        }
+
+
+        return returnValue.ToString();
     }
 }
